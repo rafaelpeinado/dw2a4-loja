5d7801dbb2e43a8ceabcca8f9c612c1474406014
rafaelpeinado <rafael.peinado.silva@gmail.com>
2019-10-21 22:21:35 -0300

    numero de itens no carrinho

---
+++ javascript/javascript.js
@@ -0,0 +1,12 @@
+function adicionarCarrinho(id) {
+    $.getJSON('json/loja.json', function (data) {
+        $.each(data, function (i, livro) {
+            if (livro.id == id) {
+                var qtde = parseInt($('#qtdeCart').text()) + 1;
+                $('#qtdeCart').text(qtde);
+                return true;
+
+            }
+        });
+    });
+}
