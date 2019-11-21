876bce4dfa27123cd2c4460eb79c02c3f0d866df
rafaelpeinado <rafael.peinado.silva@gmail.com>
2019-11-21 00:32:25 -0300

    ajuste de links

--- javascript/carrinho.js
+++ javascript/carrinho.js
@@ -18,12 +18,12 @@ $(document).ready(function (){
                                             "<span> R$ " + livro.preco + "</span>" +
                                         "</div>" +
                                     "<div id='livro"+ livro.id +"' class='quantidade-carrinho'>" + 
-                                        "<div class='menos-qtde'><a>-</a></div><div><span class='num-qtde'>" + livro.quantidade + "</span></div><div class='mais-qtde'><a>+</a></div>" +
+                                        "<div onclick='diminuirQuantidade(" + livro.id + ");' class='menos-qtde'><a href='carrinho.html'>-</a></div><div><span class='num-qtde'>" + livro.quantidade + "</span></div><div onclick='aumentarQuantidade(" + livro.id + ");' class='mais-qtde'><a href='carrinho.html'>+</a></div>" +
                                     "</div>" +
                                     "<div class='subtotal-carrinho'>" + 
                                         "<span>R$ " + (subtotal.toFixed(2)).toString().replace('.', ',') + "</span>" +
                                     "</div>" +
-                                    "<div  onclick='removerCarrinho(" + livro.id + ");' class='remover-carrinho'>" +
+                                    "<div onclick='removerCarrinho(" + livro.id + ");' class='remover-carrinho'>" +
                                         "<a href='carrinho.html'><span class='texto-remover'>x</span></a>" +
                                     "</div>" +
                                 "</div>";
@@ -48,4 +48,28 @@ function removerCarrinho(id) {
     }
     localStorage.setItem("escolhidos",JSON.stringify(novaLista));
     alert("Item removido!"); 
+}
+
+function aumentarQuantidade(id) {
+    var i = 0;
+    var escolhidos = JSON.parse(localStorage.getItem("escolhidos"));
+    for(i = 0; i < escolhidos.length; i++){
+        if(id == escolhidos[i].id){
+            escolhidos[i].quantidade++;
+        }
+    }
+    localStorage.setItem("escolhidos",JSON.stringify(escolhidos));
+}
+
+function diminuirQuantidade(id) {
+    var i = 0;
+    var escolhidos = JSON.parse(localStorage.getItem("escolhidos"));
+    for(i = 0; i < escolhidos.length; i++){
+        if(id == escolhidos[i].id){
+            if(escolhidos[i].quantidade > 1) {
+                escolhidos[i].quantidade--;
+            }
+        }
+    }
+    localStorage.setItem("escolhidos",JSON.stringify(escolhidos));
 }
