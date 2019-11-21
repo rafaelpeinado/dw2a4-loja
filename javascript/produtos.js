668d281c5c779b03b27eea0b2376a44447e93e02
rafaelpeinado <rafael.peinado.silva@gmail.com>
2019-11-20 22:13:32 -0300

    organização dos arquivos

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -6,15 +6,40 @@ if (escolhidos == null) {
 }            
 
 $(document).ready(function (){
-    $('#qtdeCart').html(carrinhoCompras.length);                  
+    $('#qtdeCart').html(carrinhoCompras.length);
+    
+    $.getJSON('json/loja.json', function(data) {
+        var item;
+        
+        $.each(data, function(i, livro) {
+            item = "<div class='produto' id='livro" + livro.id + "'>" + 
+                        "<div class='imagem'>" +
+                            "<img src='" + livro.capa + "'>" + 
+                        "</div>" + 
+                        "<div class='nome-livro'>" +
+                            "<span>" + livro.titulo + "</span>" + 
+                        "</div>" +
+                        "<div class='autor'>" +
+                                "<span>" + livro.autor + "</span>" +
+                            "</div>" +
+                        "<div class='preco'>" + 
+                            "<span> R$ " + livro.preco + "</span>" +
+                        "</div>" +
+                        "<div id='livro" + livro.id + "' class='add-carrinho'>" +
+                           "<a onclick='adicionarCarrinho(" + livro.id + ");'><span class='texto-carrinho'>Adicionar ao carrinho</span></a>" +
+                        "</div>" +
+                    "</div>";
+            $("#index").append(item);
+        });
+    });
 });
 
 function adicionarCarrinho(id) {
     var itemAdicionado = id;
     $.getJSON('json/loja.json', function (data) {
-        $.each(data, function(index, value) {
-            if(value.id == itemAdicionado) {
-                carrinhoCompras.push(value);
+        $.each(data, function(index, livro) {
+            if(livro.id == itemAdicionado) {
+                carrinhoCompras.push(livro);
                 $('#qtdeCart').html(carrinhoCompras.length);
                 localStorage.setItem("escolhidos",JSON.stringify(carrinhoCompras));                 
             }
